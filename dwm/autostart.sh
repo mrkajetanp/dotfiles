8141f9d0ebeabcd37047241440227fc48472fcdf
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-10-10 14:19:23 +0100

    dwm autostart adjustments

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -7,23 +7,12 @@ xset r rate 250 60
 nitrogen --head=0 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/mountain_lake_bright.jpg
 nitrogen --head=1 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/0079-side.jpg
 
-killall dunst
 dunst &
-
 picom &
-
-killall xbanish
 xbanish &
-
-killall lxsession
 lxsession &
-
-# wmname LG3D
-
-killall battery-notification
+greenclip daemon &
+polychromatic-tray-applet &
 /home/kajetan/.bin/battery-notification &
 
-killall greenclip
-greenclip daemon &
-
-# st &
+# wmname LG3D
---
+++ dwm/autostart_blocking.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+killall dunst
+killall xbanish
+killall lxsession
+killall battery-notification
+killall greenclip
+killall picom
+killall polychromatic-t
