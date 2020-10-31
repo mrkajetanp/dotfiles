3f0bd2994c4d5078cd7357c6ee3a3ed27262d530
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-10-31 23:18:20 +0000

    Polybar player status improvement

--- polybar/scripts/player_status.sh
+++ polybar/scripts/player_status.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-output=$(playerctl --player=spotify metadata --format "{{title}} by {{artist}}")
-echo "%{F#02c084}%{F-}" $output
+output=$(playerctl --player=spotify metadata --format "{{title}} by {{artist}}" 2>&1)
+if [[ "$output" != "No players found" ]]; then
+  echo "%{F#02c084}%{F-}" $output
+fi
