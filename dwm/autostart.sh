c11fc835ed9af14c05dfd71ba55bff7bbd8d744f
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-12-26 17:00:00 +0000

    Dwm autostart adjustment

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -5,11 +5,11 @@ setxkbmap -layout pl -variant kdvp2
 
 xset r rate 250 60
 
-#xrandr --output eDP-1 --output HDMI-2 --primary --left-of eDP-1 --output DP-1 --left-of HDMI-2 --rotate left
+# xrandr --output eDP-1 --output HDMI-2 --primary --left-of eDP-1 --output DP-1 --left-of HDMI-2 --rotate left
 
 nitrogen --head=0 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/mountain_lake_bright.jpg
-#nitrogen --head=1 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/0140.jpg
-#nitrogen --head=2 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/0079-side.jpg
+# nitrogen --head=1 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/0140.jpg
+# nitrogen --head=2 --set-scaled /home/kajetan/Pictures/Wallpapers/Brighter/0079-side.jpg
 
 dunst &
 picom &
@@ -18,5 +18,7 @@ lxsession &
 greenclip daemon &
 polychromatic-tray-applet &
 batsignal -b &
+
+alacritty &
 
 # wmname LG3D
